fac93d1a9a5e3342d347886884d390fe1a058ba7
hanrepo <hanre.polisar@yahoo.com>
2025-01-06 23:10:27 +0700

    implementing react quill

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import Logo from '../images/logo.png'
 import {FaBars} from 'react-icons/fa'
 import {AiOutlineClose} from 'react-icons/ai'
 
 const Header = () => {
+  const [isNavShowing, setIsNavShowing] = useState(window.innerWidth > 800 ? true : false)
+
+  const closeNavHandler = () => {
+    setIsNavShowing(window.innerWidth < 800 ? false : true);
+  }
+
   return (
     <nav>
       <div className='container nav__container'>
         <Link to="/" className='nav__logo'>
           <img src={Logo} alt="Navbar Logo"/>
         </Link>
-        <ul className="nav__menu">
+        {isNavShowing && <ul className="nav__menu">
           <li><Link to="/profile/sdfsdf">Ernest Archiver</Link></li>
           <li><Link to="/create">Create Post</Link></li>
           <li><Link to="/authors">Authors</Link></li>
           <li><Link to="/logout">Logout</Link></li>
-        </ul>
-        <button className='nav__toggle-btn'>
-          <AiOutlineClose/>
+        </ul>}
+        <button className='nav__toggle-btn' onClick={() => setIsNavShowing(!isNavShowing)}>
+          {isNavShowing ? <AiOutlineClose/> : <FaBars/>}
         </button>
       </div>
     </nav>
